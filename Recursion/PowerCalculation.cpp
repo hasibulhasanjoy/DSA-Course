364ce205cffe66ff8ef1421c7ccf0eb0f1bbe783
Hasibul <hasibulhasanjoy.007@gmail.com>
2023-06-10 12:43:08 +0600

    10 june 2023

--- Recursion/PowerCalculation.cpp
+++ Recursion/PowerCalculation.cpp
@@ -8,10 +8,22 @@ int power(int base, int exp)
         return base;
     return base * power(base, exp - 1);
 }
+int modularPower(int base, int exp)
+{
+    if (exp == 0)
+        return 1;
+    else if (exp == 1)
+        return base;
+    int ans = modularPower(base, exp / 2);
+    if (exp % 2 == 0)
+        return ans * ans;
+    else
+        return base * ans * ans;
+}
 int main()
 {
     int base = 5, exp = 0;
     for (int i = 0; i < 5; i++)
-        cout << power(base, i) << endl;
+        cout << modularPower(base, i) << " " << power(base, i) << endl;
     return 0;
 }
