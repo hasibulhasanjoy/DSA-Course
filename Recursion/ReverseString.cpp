445623eb4174960a13abeb12122943e9c0a46cc4
Hasibul <hasibulhasanjoy.007@gmail.com>
2023-06-10 11:47:32 +0600

    10 june 2023

--- Recursion/ReverseString.cpp
+++ Recursion/ReverseString.cpp
@@ -1,18 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-void reverse(string s, int n);
+void reverse(string &s, int i, int j)
+{
+    if (i > j)
+        return;
+    swap(s[i], s[j]);
+    reverse(s, ++i, --j);
+}
 int main()
 {
     string s = "abcdef";
     int n = s.length() - 1;
-    reverse(s, n);
-}
-void reverse(string s, int n)
-{
-    cout << s[n];
-    if (n == 0)
-    {
-        return;
-    }
-    reverse(s, n - 1);
+    reverse(s, 0, n);
+    cout << s << endl;
+    return 0;
 }
