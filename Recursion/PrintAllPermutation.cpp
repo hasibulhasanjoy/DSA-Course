0cdcb9efa2eec01edb3cb2ae88a59d1b9a04a280
Hasibul <hasibulhasanjoy.007@gmail.com>
2023-06-17 16:09:22 +0600

    Generate all permutation of a string

--- Recursion/PrintAllPermutation.cpp
+++ Recursion/PrintAllPermutation.cpp
@@ -1,38 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-void printPermutation(vector<int> nums, vector<vector<int>> &ans, int index);
-int cnt = 0;
-int main()
-{
-    vector<int> nums = {1, 2, 3};
-    vector<vector<int>> ans;
-    int index = 0;
-    printPermutation(nums, ans, index);
-    for (auto v : ans)
-    {
-        for (int i = 0; i < nums.size(); i++)
-        {
-            cout << v[i] << " ";
-        }
-        cout << endl;
-    }
-    cout << cnt << endl;
-    return 0;
-}
 
-void printPermutation(vector<int> nums, vector<vector<int>> &ans, int index)
+void generatePermutation(string &s, vector<string> &ans, int index)
 {
-    cnt++;
-    if (index == nums.size())
+    if (index >= s.length())
     {
-        ans.push_back(nums);
+        ans.push_back(s);
         return;
     }
-    for (int i = index; i < nums.size(); i++)
+    for (int i = index; i < s.size(); i++)
     {
-        swap(nums[index], nums[i]);
-        printPermutation(nums, ans, index + 1);
-        // Backtrack
-        swap(nums[index], nums[i]);
+        swap(s[index], s[i]);
+        generatePermutation(s, ans, index + 1);
+        swap(s[i], s[index]);
     }
 }
+vector<string> permutations(string &str)
+{
+    vector<string> ans;
+    generatePermutation(str, ans, 0);
+    sort(ans.begin(), ans.end());
+    return ans;
+}
+int main()
+{
+    string s = "abc";
+    vector<string> ans = permutations(s);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << endl;
+    }
+    return 0;
+}
+//? question : https://www.codingninjas.com/codestudio/problems/permutations-of-a-string_985254
+//? video : https://www.youtube.com/watch?v=va3NEycUxsg&list=PLDzeHZWIZsTryvtXdMr6rPh4IDexB5NIA&index=43&ab_channel=CodeHelp-byBabbar
