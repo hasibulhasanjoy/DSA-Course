4faf5a0d66cb7166776776622e701f27959e3338
Hasibul <hasibulhasanjoy.007@gmail.com>
2023-06-17 12:42:52 +0600

    Leetcode phone keypad problem

--- Recursion/PhoneKeyPadMapping.cpp
+++ Recursion/PhoneKeyPadMapping.cpp
@@ -1,33 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
-void phoneKeyPad(string digit, string output, int index, vector<string> &ans, string mapping[]);
-int main()
-{
-    string digit = "23";
-    string output = "";
-    int index = 0;
-    vector<string> ans;
-    string mapping[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-    phoneKeyPad(digit, output, index, ans, mapping);
-    for (auto combination : ans)
-    {
-        cout << combination << endl;
-    }
-    return 0;
-}
-void phoneKeyPad(string digit, string output, int index, vector<string> &ans, string mapping[])
+
+void getCombination(string &digit, string &output, vector<string> &ans, string *mapping, int index)
 {
     if (index == digit.length())
     {
         ans.push_back(output);
         return;
     }
-    int number = digit[index] - '0';
-    string value = mapping[number];
+    int key = digit[index] - '0';
+    string value = mapping[key];
     for (int i = 0; i < value.length(); i++)
     {
         output.push_back(value[i]);
-        phoneKeyPad(digit, output, index + 1, ans, mapping);
+        getCombination(digit, output, ans, mapping, index + 1);
         output.pop_back();
     }
 }
+
+vector<string> letterCombinations(string &digits)
+{
+    string output = "";
+    vector<string> ans;
+    string mapping[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    getCombination(digits, output, ans, mapping, 0);
+    return ans;
+}
+
+int main()
+{
+    string digit = "23";
+    vector<string> ans = letterCombinations(digit);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << endl;
+    }
+    return 0;
+}
+//? video : https://www.youtube.com/watch?v=tWnHbSHwNmA&list=PLDzeHZWIZsTryvtXdMr6rPh4IDexB5NIA&index=42&t=1191s&ab_channel=CodeHelp-byBabbar
+//? question : https://leetcode.com/problems/letter-combinations-of-a-phone-number/
