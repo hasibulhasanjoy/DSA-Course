19f1c5446b8ba57ec74f29e28536ae5fcf4ee769
Hasibul <hasibulhasanjoy.007@gmail.com>
2023-07-11 21:27:52 +0600

    reverse Linked List in k group

---
+++ Linked List/ReverseLLinKGroup.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+class LinkedList
+{
+private:
+    Node *head, *lastNode;
+    void reverseKNode(Node *start, Node *end)
+    {
+        Node *current = head;
+        Node *previous = NULL;
+        Node *NextNode = head;
+        while (previous != end)
+        {
+            NextNode = current->next;
+            current->next = previous;
+            previous = current;
+            current = NextNode;
+        }
+        start = previous;
+    }
+    Node *reverseKGroup(Node *head, int k)
+    {
+        if (head == NULL or head->next == NULL or k == 1)
+        {
+            return head;
+        }
+        Node *start = head;
+        Node *end = head;
+        int count = 1;
+        while (count < k)
+        {
+            end = end->next;
+            if (end->next == NULL)
+            {
+                return head;
+            }
+            count++;
+        }
+        Node *nextHead = reverseKGroup(end->next, k);
+        reverseKNode(start, end);
+        start->next = nextHead;
+        return end;
+    }
+
+public:
+    LinkedList()
+    {
+        head = NULL;
+        lastNode = NULL;
+    }
+    void addNode(int data)
+    {
+        Node *newNode = new Node;
+        newNode->data = data;
+        newNode->next = NULL;
+        if (head == NULL)
+        {
+            head = newNode;
+            lastNode = newNode;
+            return;
+        }
+        lastNode->next = newNode;
+        lastNode = lastNode->next;
+    }
+    void reverseInKGroup(int k)
+    {
+        head = reverseKGroup(head, k);
+    }
+    void print()
+    {
+        Node *current = head;
+        while (current != NULL)
+        {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+};
+int main()
+{
+    LinkedList l;
+    for (int i = 1; i <= 10; i++)
+    {
+        l.addNode(i);
+    }
+    l.print();
+    l.reverseInKGroup(2);
+    l.print();
+    return 0;
+}
+
+//? Video : https://rb.gy/32shq
+//? Video : https://www.youtube.com/watch?v=TeDcLjOOiK4&ab_channel=Fraz
